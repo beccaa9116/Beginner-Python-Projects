165284c728050486ee1632a5b67725d88cad01fe
Becca <rebeka.borsikova@gmail.com>
2023-02-26 17:59:37 +0100

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+influencer_name = input("Your fav influencer: ")
+platform = input("Platform of your favourite influencer: ")
+time = input("How long you've been following this person: ")
+
+madlib = f"My favourite person on the internet is {influencer_name}. There is literally no better person on {platform}! I have been following this creator for {time} and I'm not stopping anytime soon!"
+print(madlib)
